f2590f512e4f4f99dd5307ebf44d18787d76348a
Tommy18062001 <johantony63@gmail.com>
2022-09-24 16:42:56 +0300

    fix cors issue to connect with frontend

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ app
 .use(bodyParser.json())
 .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   })
 .use('/contacts', contactsRoute)
